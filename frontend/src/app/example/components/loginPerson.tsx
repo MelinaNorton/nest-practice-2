1fee123e31c98b076e3de879235f0cd4eacf12aa
MelinaNorton <melinan@clemson.edu>
2025-05-26 16:30:44 +0200

    implemented rudimentary login form connecting to the auth/login endpoint

---
+++ frontend/src/app/example/components/loginPerson.tsx
@@ -0,0 +1,55 @@
+'use client'
+import React, { useState }  from "react";
+import axios from "axios";
+
+const LoginPerson = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPass] = useState("");
+    const [response, setResponse] = useState("");
+
+    const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const loginData = {
+            username,
+            password
+        }
+
+        axios
+            .patch("http://localhost:33333/auth/login", loginData)
+            .then((response) =>{
+                setResponse("Successful Post!");
+                setUsername("");
+                setPass("");
+            })
+            .catch((error) => {
+                setResponse("Unsuccessful Post :(!");
+            });
+        
+    }
+
+    return (
+        <div className="max-w-md flex-row font-semibold border border-black divide-y divide-black">
+            <form onSubmit ={handleLogin}>
+                <input 
+                id="Loginusername"
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                />
+                <input 
+                id="Loginpassword"
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPass(e.target.value)}
+                />
+                <button type="submit" className="shadow-md bg-sky-400 hover:bg-sky-200">Submit</button>
+                <p>{response}</p>
+            </form>
+        </div>
+    );
+}
+
+export default LoginPerson;
--- frontend/src/app/example/page.tsx
+++ frontend/src/app/example/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import AddPerson from "./components/postPerson";
 import PatchName from './components/patchPerson';
+import LoginPerson from "./components/loginPerson"
+
 export default function Page() {
     return(
     <main>
       <h1>Welcome!</h1>
       <AddPerson />
       <PatchName />
+      <LoginPerson />
     </main>
     );
 }
